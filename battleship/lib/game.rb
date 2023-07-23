837c683342b2e964dd64d0cdf3b18814838870f6
Cory Powell <coryrpowell@gmail.com>
2023-07-22 20:17:49 -0400

    begin game_placement method

--- battleship/lib/game.rb
+++ battleship/lib/game.rb
@@ -21,7 +21,6 @@ class Game
         @player_1.render(true)
         @computer.render
 
-
       elsif answer == "q"
         puts "Ok, see you next time."
       else 
@@ -30,4 +29,22 @@ class Game
     end
   end
 
+  def game_placement
+    puts "I have laid out my ships on the grid.
+    You now need to lay out your two ships.
+    The Cruiser is three units long and the Submarine is two units long.
+      1 2 3 4
+    A . . . .
+    B . . . .
+    C . . . .
+    D . . . .
+    Enter the squares for the Cruiser (3 spaces)"
+    cruiser_array = @computer.possible_cruiser.sample
+    sub_array = @computer.possible_sub.sample
+
+    @computer.place(cruiser, cruiser_array)
+
+    # maybe need to refactor the computer method for 
+    # ship placement in the Board class? 
+  end
 end
